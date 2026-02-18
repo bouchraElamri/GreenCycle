ccc8b18989682d095d5e4e83d21ac44c5ff8622e
Abdelmoughit Taftaf <taftafabdelmoughit@gmail.com>
2026-02-18 13:17:32 +0000

    Revert "footer done, navbar still without pdp api"
    This reverts commit fe8c6947d3ce2f3195434e53bb608f435739341c.

--- frontend/src/pages/public/HomePage.jsx
+++ frontend/src/pages/public/HomePage.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Navbar from '../../components/layouts/Navbar';
-import Footer from '../../components/layouts/Footer';
 const HomePage = () => {
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div>
       <Navbar />
-      <div className='flex-1'>HomePage</div>
-      <Footer />
+      <div>HomePage</div>
     </div>
   )
 }
