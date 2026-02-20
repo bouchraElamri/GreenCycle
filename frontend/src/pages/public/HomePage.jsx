1a9d0e942ae68649fcf688b1577d9842a52361ca
Abdelmoughit Taftaf <taftafabdelmoughit@gmail.com>
2026-02-20 12:02:32 +0000

    justing commiting to merge layout with the main

--- frontend/src/pages/public/HomePage.jsx
+++ frontend/src/pages/public/HomePage.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
-import Navbar from '../../components/layouts/Navbar';
-import Footer from '../../components/layouts/Footer';
 
 const HomePage = () => {
-  return (
-    <div className='min-h-screen flex flex-col'>
-      <Navbar />
-      homepage
-      <Footer />
-    </div>
-  )
+  
 }
 
 export default HomePage
