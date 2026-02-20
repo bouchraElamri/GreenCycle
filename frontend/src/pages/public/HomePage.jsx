d155fc646d152fe3d5537de1eb1c81eafe149502
Abdelmoughit Taftaf <taftafabdelmoughit@gmail.com>
2026-02-20 02:22:21 +0000

    hero section created but not done

--- frontend/src/pages/public/HomePage.jsx
+++ frontend/src/pages/public/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/layouts/Navbar';
 import Footer from '../../components/layouts/Footer';
+import Hero from '../../components/LandingPage/hero';
 const HomePage = () => {
   return (
     <div className='min-h-screen flex flex-col'>
       <Navbar />
-      <button className='flex-1 lg:text-white lg:mt-28'> click here</button>
+      <Hero/>
       <Footer />
     </div>
   )
