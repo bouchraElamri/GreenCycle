a3364f87b0752195ad34beaf35101122e245802c
Abdelmoughit Taftaf <taftafabdelmoughit@gmail.com>
2026-02-20 13:15:26 +0000

    navbar still one modif

--- frontend/src/pages/public/HomePage.jsx
+++ frontend/src/pages/public/HomePage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import MainLayout from '../../components/layouts/MainLayout'
 const HomePage = () => {
-  
+  return (
+    <MainLayout>
+      <div>HomePage</div>
+    </MainLayout>
+  )
 }
 
 export default HomePage
